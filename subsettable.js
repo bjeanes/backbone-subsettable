26028df9c2b1497f194acfeacce365596a51ec68
Bodaniel Jeanes <me@bjeanes.com>
2011-11-19 23:35:50 -0600

    More subsettable stuff

--- subsettable.js
+++ subsettable.js
@@ -1,13 +1,42 @@
 extend('Backbone.SubsettableCollection', {
   subset: function(filter) {
-    // subset = new (Backbone.Collection.extend(this))(this.filter(filter));
-
     filter || (filter = function() { return true; });
 
-    subset = _.extend(this);
-    models = this.filter(filter);
+    var superset    = this;
+    var SubsetClass = Backbone.Collection.extend(superset);
+    var subset      = new SubsetClass(superset.filter(filter));
 
-    subset.reset(models);
+    subset.add = function(models, options) {
+      models = _.isArray(models) ? models : [models];
+
+      _.each(models, function(model) {
+        if(superset.contains(model) && filter(model)) {
+          subset._add(model, options);
+        }
+      });
+    };
+
+    subset.remove = function(models, options) {
+      models = _.isArray(models) ? models : [models];
+
+      _.each(models, function(model) {
+        subset._remove(model, options);
+      });
+    };
+
+    var change = function(model) {
+      var matches = filter(model);
+
+      if(matches && !subset.contains(model)) {
+        subset.add(model);
+      } else {
+        subset.remove(model);
+      }
+    };
+
+    superset.bind('change', change,        subset);
+    superset.bind('add',    subset.add,    subset);
+    superset.bind('remove', subset.remove, subset);
 
     return subset;
   }
