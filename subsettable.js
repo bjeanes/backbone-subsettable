27cfea79daad1c6c4db02764cb2bb6940e32329f
Bodaniel Jeanes <me@bjeanes.com>
2011-11-20 19:32:16 -0600

    Cleanup and add TODO for issue that wasted all of my day.

--- subsettable.js
+++ subsettable.js
@@ -1,3 +1,19 @@
+// Not sure where to put this note yet but putting it here for now
+// because it wasted 24 hours of my life.
+//
+// If you have multiple different subset collections of a single superset
+// and change a model such that it moves between them, the @remove@ event
+// can be triggered after the @add@ event (because the @change@ event is
+// triggered for each subset in an abritrary order, and it will trigger a
+// @add@ event in one and a @remove@ event in another). Since a @remove@
+// event in a collection will trigger the model's @remove@ event (as it should),
+// *ALL* views that are listening for that event on that model will be fired,
+// even if the view represents the collection to which we are adding the model.
+// The consequence of this is that all views representing the changed models
+// are removed from the DOM, even though they are still in the collection.
+//
+// To avoid this, make sure your view's event listeners check the collection
+// from which the event originated before actually removing the view.
 extend('Backbone.SubsettableCollection', {
   subset: function(filter) {
     filter || (filter = function() { return true; });
@@ -5,13 +21,22 @@ extend('Backbone.SubsettableCollection', {
     var B = window.Backbone || window.vendor.Backbone;
     var _ = window._        || B._;
 
-    var superset = this;
-    var subset   = new superset.__proto__.constructor(superset.filter(filter)); // nicer way?
 
-    _.bindAll(subset, 'add', 'remove', 'reset');
+    Object.getPrototypeOf || (Object.getPrototypeOf = function(obj) {
+      return obj.__proto__ || obj.constructor.prototype;
+    });
+
+    var constructor = function(obj) {
+      return obj.constructor || Object.getPrototypeOf(obj).constructor;
+    }
+
+    var superset = this;
+    var Subset   = constructor(superset);
+    var subset   = new Subset(superset.filter(filter));
+
+    _.bindAll(subset, 'add', 'remove');
     var add    = subset.add;
     var remove = subset.remove;
-    var reset  = subset.reset;
 
     var flattenedEach = function(items, fn) {
       items = _.isArray(items) ? items : [items];
@@ -28,14 +53,22 @@ extend('Backbone.SubsettableCollection', {
 
     subset.remove = function(models, options) {
       flattenedEach(models, function(model) {
-        if(!(superset.contains(model) && filter(model))) {
+        var matches    = filter(model),
+            inSuperset = superset.contains(model),
+            inSubset   = subset.contains(model);
+
+        if(inSuperset) {
+          if(!matches && inSubset) {
+            remove(model, options);
+          }
+        } else {
           remove(model, options);
         }
       });
     };
 
-    subset.reset = function(collection) {
-      reset(collection.models);
+    var reset = function(collection) {
+      subset.reset(collection.models);
     };
 
     var change = function(model) {
@@ -43,18 +76,19 @@ extend('Backbone.SubsettableCollection', {
 
       if(matches && !subset.contains(model)) {
         subset.add(model);
-      } else {
+        return;
+      }
+
+      if(!matches && subset.contains(model)) {
         subset.remove(model);
+        return;
       }
     };
 
     superset.bind('change', change,        subset);
     superset.bind('add',    subset.add,    subset);
     superset.bind('remove', subset.remove, subset);
-    superset.bind('reset',  subset.reset,  subset);
-    superset.bind('all', function(event) {
-      console.log("DEBUG superset event", event, arguments);
-    });
+    superset.bind('reset',  reset,         subset);
 
     return subset;
   }
